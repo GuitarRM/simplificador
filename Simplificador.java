1c725eb4957dac6831ec86eb4164193a3c6d8aca
Fernando Isnaldo Silva de Faria <33297740+fernandoisnaldo@users.noreply.github.com>
2020-02-15 20:02:13 -0300

    Código-fonte do simplificador de frações.

---
+++ Simplificador.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2020 Fernando Isnaldo Silva de Faria
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <httpo://www.gnu.org/licenses/>.
+ */
+package com.mycompany.simplificadordefracoes;
+
+import javax.swing.JOptionPane;
+
+/**
+ * fernando-i@outlook.com
+ * @author Fernando Isnaldo Silva de Faria
+ */
+public class Simplificador {
+    public static void main(String Args[]){
+        float numerador, denominador;
+        int mmc;
+        boolean negativo;
+        JOptionPane.showMessageDialog(null, "Atenção: O programa a seguir, é um projeto de fundo de quintal! \nLeia com atenção as próximas caixas de diálogo, senão vai dar erro.");
+        numerador=Float.parseFloat(JOptionPane.showInputDialog("Insira somente o numerador inteiro"));
+        denominador=Float.parseFloat(JOptionPane.showInputDialog("Insira somente o denominador inteiro"));
+        mmc=2;
+        negativo=false;
+        if(numerador<0){
+            numerador=numerador*-1;
+            negativo=true;
+        }
+        while(mmc <=numerador && mmc <= denominador){
+            if(numerador%mmc!=0 || denominador%mmc!=0){
+                mmc=mmc+1;
+            }
+            else{
+                numerador=numerador/mmc;
+                denominador=denominador/mmc;
+                mmc=2;
+            }
+        }
+        if(negativo==true){
+            numerador=numerador*-1;
+        }
+        if(numerador==0){
+            JOptionPane.showMessageDialog(null, "O resultado é 0.");
+        }
+        else if(denominador==1){
+            JOptionPane.showMessageDialog(null, "O resultado é um número inteiro: " + (int)numerador);
+        }
+        else if(denominador%1!=0 || numerador%1!=0){
+            JOptionPane.showMessageDialog(null, "Transforme os números em uma fração");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "A fração simplificada é: " + (int)numerador + "/" + (int)denominador);
+        }
+        System.exit(0);
+    }
+}
